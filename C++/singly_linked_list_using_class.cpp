4e7672cb8b7c9d50e6126a22d7ea8ab0785ccc1c
soumadip2001 <soumadip.pal.30@gmail.com>
2023-07-04 00:55:36 +0530

    modified singly linked list using class[C

--- C++/singly_linked_list_using_class.cpp
+++ C++/singly_linked_list_using_class.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 //node class used to represnt a node of linked list
-class node{
+class Node{
 	public:
 		int data;
-		Node * next;
+		Node *next;
 		Node(){
 			data=0;
 			next=NULL;
@@ -19,20 +19,21 @@ class node{
 //linked list class to implement a linkedlist
 class linkedlist{
 	Node* head;
-	linkedlist(){
-		head=NULL;
-	}
-	//data to be entered
-	void insertNode(int);
-	//position to be entered
-	void deleteNode(int);
-	void printList();
+	public:
+		linkedlist(){
+			head=NULL;
+		}
+		//data to be entered
+		void insertNode(int);
+		//position to be entered
+		void deleteNode(int);
+		void printList();
 };
 void linkedlist::deleteNode(int pos){
 	Node *temp1=head,*temp2=NULL;
 	int len=0;
 	if(head==NULL){
-		cout<<"Empty linked list";
+		cout<<"Empty linked list"<<"\n";
 		return ;
 	}
 	while(temp1!=NULL){
@@ -40,30 +41,61 @@ void linkedlist::deleteNode(int pos){
 		len++;
 	}
 	if(len<pos){
-		cout<<"position exceed";
+		cout<<"index out of range "<<"\n";
 		return;
 	}
 	
 	if(pos==1){
 		head=head->next;
-		delete head;
+		delete temp1;
 		return;
 	}
 	
 	temp1=head;
-	while(len--){
+	while(pos-- > 1){
 		temp2=temp1;
 		temp1=temp1->next;
 	}
 	temp2->next=temp1->next;
 	delete temp1;
-	return;
 }
 void linkedlist::insertNode(int val){
+	Node* n1=new Node(val); 
 	if(head==NULL){
-		head->data=val
+		head=n1;
+		return;
+	}
+	Node* temp1=head,*temp2=NULL;
+	while(temp1!=NULL){
+		temp2=temp1;
+		temp1=temp1->next;
+	}
+	temp2->next=n1;
+	cout<<"insert ";
+//	return ;
+}
+void linkedlist::printList(){
+	Node *temp1=head;
+	if(head==NULL){
+		cout<<"NO node is present ";
+		return;
+	}
+	while(temp1!=NULL){
+		cout<<temp1->data<<" ";
+		temp1=temp1->next;
 	}
 }
 int main(){
+	linkedlist list;	//object- list
+	list.insertNode(1);
+	list.insertNode(2);
+	list.insertNode(3);
+	list.insertNode(4);
+	list.insertNode(5);
+	list.insertNode(6);
+	list.printList();
+	list.deleteNode(3);
+	list.printList();
+	return 0;
 	
 }
