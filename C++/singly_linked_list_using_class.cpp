2973e189a4c3fa13336e5d49de6ed21e39699b75
soumadip2001 <soumadip.pal.30@gmail.com>
2023-07-05 01:16:39 +0530

    reverse linked list and middle of linked list implemented by two approach

--- C++/singly_linked_list_using_class.cpp
+++ C++/singly_linked_list_using_class.cpp
@@ -28,6 +28,9 @@ class linkedlist{
 		//position to be entered
 		void deleteNode(int);
 		void printList();
+		void reverse_linked_list();
+		void middle_element1();		//middle element by approach 1
+		void middle_element2();		//middle element by approach 2 by slow and fast pointer
 };
 void linkedlist::deleteNode(int pos){
 	Node *temp1=head,*temp2=NULL;
@@ -71,19 +74,69 @@ void linkedlist::insertNode(int val){
 		temp1=temp1->next;
 	}
 	temp2->next=n1;
-	cout<<"insert ";
+	cout<<"\n";
 //	return ;
 }
 void linkedlist::printList(){
 	Node *temp1=head;
 	if(head==NULL){
-		cout<<"NO node is present ";
+		cout<<"NO node is present "<<"\n";
 		return;
 	}
 	while(temp1!=NULL){
 		cout<<temp1->data<<" ";
 		temp1=temp1->next;
 	}
+	cout<<"\n";
+}
+void linkedlist::reverse_linked_list(){
+	Node *temp=head;
+	if(head==NULL){
+		cout<<"no element in the list "<<"\n";
+		return;
+	}
+	Node *curr=head,*prev=NULL;
+	while(temp!=NULL){
+		curr=temp;
+		temp=temp->next;
+		curr->next=prev;
+		prev=curr;
+	}
+	head=curr;
+	while(curr!=NULL){
+		cout<<curr->data<<" ";
+		curr=curr->next;
+	}
+	cout<<"\n";
+}
+void linkedlist::middle_element1(){
+	if(head==NULL){
+		cout<<"no element found "<<"\n";
+		return ;
+	}
+	Node *temp=head;
+	int len=0;
+	while(temp!=NULL){
+		temp=temp->next;
+		len++;
+	}
+	temp=head;
+	int mid=(len/2)+(len%2);
+	if(mid%2){
+		mid+=1;
+	}
+	while(mid-->1){
+		temp=temp->next;
+	}
+	cout<<"middle element "<<temp->data;
+}
+void linkedlist::middle_element2(){
+	Node *slow=head,*fast=head;
+	while(fast && fast->next){
+		slow=slow->next;
+		fast=fast->next->next;
+	}
+	cout<<"middle element by tortoise method "<<slow->data;
 }
 int main(){
 	linkedlist list;	//object- list
@@ -93,9 +146,14 @@ int main(){
 	list.insertNode(4);
 	list.insertNode(5);
 	list.insertNode(6);
+	list.insertNode(7);
 	list.printList();
 	list.deleteNode(3);
 	list.printList();
+	list.reverse_linked_list();	//reversed linked list
+	list.reverse_linked_list();
+	list.middle_element1();
+	list.middle_element2();
 	return 0;
 	
 }
